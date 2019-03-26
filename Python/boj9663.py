261a51e75bec563b010f60b0b9c03aad5dced097
Changmin Choi <cmchoi9901@gmail.com>
2019-03-27 00:22:04 +0900

    boj.kr/9663 solved

--- Python/boj9663.py
+++ Python/boj9663.py
@@ -5,14 +5,17 @@ de_unable = set() # unable set of decreasing
 row = 0 # row number.
 latest_top = -1
 if __name__ == "__main__":
+    count = 0
     i = int(input())
     unused_num = set(range(i))
-    while(len(num_list) != i):
+    while(not(len(num_list) == 0 and latest_top == i-1)):
         available_num = sorted([a for a in unused_num if a - row not in in_unable and a + row not in de_unable and a > latest_top])
         if(len(available_num) == 0):
+            if(len(unused_num) == 0):
+                count += 1
             row -= 1
-            top = num_list[len(num_list) - 1]
-            num_list = num_list[:len(num_list) - 1]
+            top = num_list[row]
+            num_list = num_list[:row]
             in_unable -= set([top - row])
             de_unable -= set([top + row])
             unused_num |= set([top])
@@ -24,5 +27,4 @@ if __name__ == "__main__":
             unused_num -= set([available_num[0]])
             row += 1
             latest_top = -1
-    for b in num_list:
-        print(b + 1)
+    print(count)

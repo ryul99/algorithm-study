399d7cdd7e60b0e77a528f2a52ddfa5c19fb28d7
Changmin Choi <cmchoi9901@gmail.com>
2019-04-18 16:57:27 +0900

    boj.kr/8393 cpp

---
+++ C++/boj8393.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+//8393
+int main(int argc, char const *argv[]) {
+    int N;
+    scanf("%d", &N);
+    printf("%d\n", N * (N+1) / 2);
+    return 0;
+}

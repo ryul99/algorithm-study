af347eb7988f23642b416c10ca570f6c99de29be
Changmin Choi <cmchoi9901@gmail.com>
2019-04-18 17:07:26 +0900

    boj.kr/11654 cpp

---
+++ C++/boj11654.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+//11654
+int main(int argc, char const *argv[]) {
+    char a;
+    scanf("%c", &a);
+    printf("%d\n", a);
+    return 0;
+}

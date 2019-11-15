c3dd106912c9212b0628a99e1b76868791cd7cb9
Changmin Choi <cmchoi9901@gmail.com>
2019-11-15 20:02:02 +0900

    temp

--- C++/boj/boj12865.cpp
+++ C++/boj/boj12865.cpp
@@ -10,7 +10,7 @@
 #include <queue>
 #include <cmath>
 
-int dp[110]; //dp[i]: i is accumulated weight
+int dp[100100]; //dp[i]: i is (accumulated weight)
 
 using namespace std;
 //https://www.acmicpc.net/problem/12865
@@ -24,7 +24,16 @@ int main(int argc, char const *argv[]) {
         all_obj.emplace_back(a,b);
     }
     sort(all_obj.begin(), all_obj.end());
-    for()
-    max(dp[w-a] + b, dp[w])
+    for(auto p: all_obj){
+        int w = p.first;
+        int v = p.second;
+        for(int i = w; i < 100100; i++){
+            dp[i] = max(dp[i - w] + v, dp[i]);
+        }
+    }
+    int mx = 0;
+    for(int i = 0; i <= K; i++)
+        mx = max(mx, dp[i]);
+    printf("%d\n", mx);
     return 0;
 }

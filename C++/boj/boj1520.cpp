ab2624718d333480e87323d65804397bb9caf455
Changmin Choi <cmchoi9901@gmail.com>
2019-11-19 10:22:02 +0900

    C++ boj1520 solved

--- C++/boj/boj1520.cpp
+++ C++/boj/boj1520.cpp
@@ -13,29 +13,39 @@
 int inn[550][550];
 int dp[550][550];
 
-void move(int n, int m){
-    dp[n][m]++;
-    if(n + 1 < 550 && inn[n + 1][m] < inn[n][m])
-        move(n + 1, m);
-    if(m + 1 < 550 && inn[n][m + 1] < inn[n][m])
-        move(n, m + 1);
-    if(n - 1 > 0 && inn[n - 1][m] < inn[n][m])
-        move(n - 1, m);
-    if(m - 1 > 0 && inn[n][m - 1] < inn[n][m])
-        move(n, m - 1);
-    return;
-}
-
 using namespace std;
 //https://www.acmicpc.net/problem/1520
 int main(int argc, char const *argv[]) {
     int N, M;
+    vector<pair<int, pair<int, int> > > v;
     scanf("%d %d", &M, &N);
     for(int i = 0; i < M; i++){
-        for(int j = 0; j < N; j++)
+        for(int j = 0; j < N; j++){
             scanf("%d", &inn[i][j]);
+            v.push_back(make_pair(inn[i][j], make_pair(i, j)));
+        }
     }
-    move(0, 0);
+    sort(v.begin(), v.end());
+    dp[0][0] = 1;
+    while(!v.empty()){
+        auto t = v.back();
+        int value = t.first;
+        int i = t.second.first;
+        int j = t.second.second;
+        if(i + 1 < 550 && inn[i + 1][j] > inn[i][j]){
+            dp[i][j] += dp[i + 1][j];
+        }
+        if(j + 1 < 550 && inn[i][j + 1] > inn[i][j]){
+            dp[i][j] += dp[i][j + 1];
+        }
+        if(i - 1 >= 0 && inn[i - 1][j] > inn[i][j]){
+            dp[i][j] += dp[i - 1][j];
+        }
+        if(j - 1 >= 0 && inn[i][j - 1] > inn[i][j]){
+            dp[i][j] += dp[i][j - 1];
+        }
+        v.pop_back();
+    }
     printf("%d\n", dp[M - 1][N - 1]);
     return 0;
 }

69a168b34660d1eaa664c7ebf91b3257c8548e68
Changmin Choi <cmchoi9901@gmail.com>
2021-11-09 02:01:06 +0900

    boj1550 cpp solved

---
+++ C++/boj/boj1550.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+//https://www.acmicpc.net/problem/1550
+int main(int argc, char const *argv[]) {
+    long long n;
+    scanf("%llx", &n);
+    printf("%lld\n", n);
+    return 0;
+}

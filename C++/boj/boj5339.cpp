4c41164e4207e7018456179116a503d43e670b52
Changmin Choi <cmchoi9901@gmail.com>
2022-01-25 00:42:42 +0900

    boj5339 cpp solved

---
+++ C++/boj/boj5339.cpp
@@ -0,0 +1,30 @@
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+//
+int main(int argc, char const *argv[]) {
+    //ios_base::sync_with_stdio(false)
+    printf("     /~\\\n"
+"    ( oo|\n"
+"    _\\=/_\n"
+"   /  _  \\\n"
+"  //|/.\\|\\\\\n"
+" ||  \\ /  ||\n"
+"============\n"
+"|          |\n"
+"|          |\n"
+"|          |\n");
+    return 0;
+}

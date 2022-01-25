3ab9cd0f64d1cb6cbc9d644db1da9829d51d420f
Changmin Choi <cmchoi9901@gmail.com>
2022-01-25 16:12:43 +0900

    boj9654 cpp solved

---
+++ C++/boj/boj9654.cpp
@@ -0,0 +1,26 @@
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+//https://www.acmicpc.net/problem/9654
+int main(int argc, char const *argv[]) {
+    //ios_base::sync_with_stdio(false)
+    printf("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n\
+N2 Bomber      Heavy Fighter  Limited    21        \n\
+J-Type 327     Light Combat   Unlimited  1         \n\
+NX Cruiser     Medium Fighter Limited    18        \n\
+N1 Starfighter Medium Fighter Unlimited  25        \n\
+Royal Cruiser  Light Combat   Limited    4         \n");
+    return 0;
+}

a2a58db6ac7ef0925dcb928e4f853b6648333e54
Changmin Choi <cmchoi9901@gmail.com>
2022-01-27 22:09:53 +0900

    boj10926 cpp solved

---
+++ C++/boj/boj10926.cpp
@@ -0,0 +1,23 @@
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+//https://www.acmicpc.net/problem/10926
+int main(int argc, char const *argv[]) {
+    //ios_base::sync_with_stdio(false)
+    char s[55];
+    scanf(" %s", s);
+    printf("%s\?\?!\n", s);
+    return 0;
+}

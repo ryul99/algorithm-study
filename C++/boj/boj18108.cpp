0a5890717f12dc86efbae637fd9c784c69a300bc
Changmin Choi <cmchoi9901@gmail.com>
2022-01-31 12:55:31 +0900

    boj18108 cpp solved

---
+++ C++/boj/boj18108.cpp
@@ -0,0 +1,23 @@
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+//https://www.acmicpc.net/problem/18108
+int main(int argc, char const *argv[]) {
+    //ios_base::sync_with_stdio(false)
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", a-543);
+    return 0;
+}

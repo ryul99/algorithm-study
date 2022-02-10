0bb9bab57921446624b22f1210fe95574299d153
Changmin Choi <cmchoi9901@gmail.com>
2022-02-10 21:38:15 +0900

    boj2442 cpp solved

---
+++ C++/boj/boj2442.cpp
@@ -0,0 +1,29 @@
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+//https://www.acmicpc.net/problem/2442
+int main(int argc, char const *argv[]) {
+    //ios_base::sync_with_stdio(false)
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N - i - 1; ++j)
+            printf(" ");
+        for (int j = 0; j < 2 * i + 1; ++j)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}

ab53ac777396f9486b302cecf9734949d56d45d3
Changmin Choi <cmchoi9901@gmail.com>
2023-02-19 01:58:29 +0900

    add cin.tie(NULL) to base.cpp

--- C++/base.cpp
+++ C++/base.cpp
@@ -15,7 +15,7 @@
 using namespace std;
 //
 int main(int argc, char const *argv[]) {
-    //ios_base::sync_with_stdio(false)
+    //ios_base::sync_with_stdio(false); cin.tie(NULL);
 
     return 0;
 }

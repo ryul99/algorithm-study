36cf53833a1235a5839e578c521050b7068bab2e
Changmin Choi <cmchoi9901@gmail.com>
2023-02-19 03:43:24 +0900

    boj 1620 cpp solved

--- C++/boj/boj1620.cpp
+++ C++/boj/boj1620.cpp
@@ -1,19 +1,39 @@
-#include <stdio.h>
+#include <cstring>
+#include <string>
+#include <cstdio>
 #include <iostream>
 #include <vector>
+#include <map>
 #include <utility>
 #include <algorithm>
 #include <set>
+#include <unordered_set>
 #include <stack>
+#include <queue>
+#include <cmath>
+
 using namespace std;
-//1620
+// boj.kr/1620
 int main(int argc, char const *argv[]) {
-  char[30] name;
-  int pokeN, QN;
-  unordered_set<string> poketmon;
-  scanf("%d %d", &pokeN, &QN);
-  for(int i = 0; i < pokeN; i++){
-    scanf("%")
-  }
-  return 0;
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int N, M;
+    string s;
+    cin >> N >> M;
+    vector<string> i2s(N + 10);
+    map<string, int> s2i;
+    for (int i = 0; i < N; ++i) {
+        cin >> s;
+        i2s[i + 1] = s;
+        s2i[s] = i + 1;
+    }
+    for (int i = 0; i < M; ++i) {
+        cin >> s;
+        int num = atoi(s.c_str());
+        if (num == 0) {
+            cout << s2i[s] << '\n';
+        } else {
+            cout << i2s[num] << '\n';
+        }
+    }
+    return 0;
 }
